f5e5946d051be72483e7c3ab7ff1ad350872541f
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2024-02-22 10:00:42 +0100

    Solución 1.0.i con objetos (en clase)

---
+++ Clase2024/src/tema1/resueltos/Ejercicio1_0iConObjetos.java
@@ -0,0 +1,58 @@
+package tema1.resueltos;
+
+public class Ejercicio1_0iConObjetos {
+	//	Crea dos arrays, uno con los nombres de 4 o 5 usuarios de cualquier red social que uses,
+	//	y otro con sus seguidores. Realiza un programa que muestre por consola esos usuarios 
+	//  primero sin ordenar y luego ordenados por número de seguidores 
+	//	(de mayor a menor).
+
+	public static void main(String[] args) {
+		// String[] usuarios = { "@sama", "@JeffBezos", "@BillGates", "@elonmusk" };
+		// int[] seguidores = { 1300, 61000, 62000, 128900 };
+		UsuarioRedSocial[] usuariosRS = new UsuarioRedSocial[4];
+		// usuariosRS[0] = new UsuarioRedSocial();
+		// usuariosRS[0].nombre = "@sams";
+		// usuariosRS[0].numSeguidores = 1300;
+		// usuariosRS[1] = new UsuarioRedSocial();
+		// usuariosRS[1].nombre = "@JeffBezos";
+		// usuariosRS[1].numSeguidores = 61000;
+		// usuariosRS[2] = new UsuarioRedSocial();
+		// usuariosRS[2].nombre = "@BillGates";
+		// usuariosRS[2].numSeguidores = 62000;
+		// usuariosRS[3] = new UsuarioRedSocial();
+		// usuariosRS[3].nombre = "@elonmusk";
+		// usuariosRS[3].numSeguidores = 128900;
+		usuariosRS[0] = new UsuarioRedSocial( "@sams", 1300 );
+		usuariosRS[1] = new UsuarioRedSocial( "@JeffBezos", 61000 );
+		usuariosRS[2] = new UsuarioRedSocial( "@BillGates", 62000 );
+		usuariosRS[3] = new UsuarioRedSocial( "@elonmusk", 128900 );
+
+		visualizarUsuariosYSeguidores( usuariosRS );
+		ordenaUsuariosPorSeguidores( usuariosRS );
+		visualizarUsuariosYSeguidores( usuariosRS );
+	}
+	
+	// Visualiza línea a línea usuario tabulador nº seguidores
+	private static void visualizarUsuariosYSeguidores(UsuarioRedSocial[] usuarios ) {
+		for (int i=0; i<usuarios.length; i++) {
+			if (usuarios[i] != null) {
+				System.out.println( usuarios[i].nombre + "\t" + usuarios[i].numSeguidores );
+			}
+		}
+	}
+	
+	private static void ordenaUsuariosPorSeguidores( UsuarioRedSocial[] usuarios ) {
+		for (int pasada=0; pasada<usuarios.length-1; pasada++) {
+			for (int comp=0; comp<usuarios.length-1; comp++) {  // TODO mejorar que solo se hagan las comparaciones necesarias
+				// boolean hayQueIntercambiar = usuarios[comp].numSeguidores < usuarios[comp+1].numSeguidores;
+				boolean hayQueIntercambiar = usuarios[comp].tieneMenosSeguidoresQue( usuarios[comp+1] );
+				if (hayQueIntercambiar) {
+					UsuarioRedSocial auxNombre = usuarios[comp];
+					usuarios[comp] = usuarios[comp+1];
+					usuarios[comp+1] = auxNombre;
+				}
+			}
+		}
+	}
+	
+}
---
+++ Clase2024/src/tema1/resueltos/UsuarioRedSocial.java
@@ -0,0 +1,26 @@
+package tema1.resueltos;
+
+public class UsuarioRedSocial {
+    public String nombre = "";
+    public int numSeguidores = 0; // da igual = 0 que nada
+
+    public UsuarioRedSocial( String nombre, int numSeguidores ) {
+        this.nombre = nombre;
+        this.numSeguidores = numSeguidores;
+    }
+
+    public UsuarioRedSocial( String nombre ) {
+        this.nombre = nombre;
+        this.numSeguidores = 0;
+    }
+
+// No se podría
+//    public UsuarioRedSocial( String url ) {
+        //
+//    }
+
+    public boolean tieneMenosSeguidoresQue( UsuarioRedSocial u2 ) {
+        return this.numSeguidores < u2.numSeguidores;
+    }
+
+}
