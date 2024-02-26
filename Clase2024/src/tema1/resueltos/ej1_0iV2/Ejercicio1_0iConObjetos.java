9178a48bfffa063c3b23d0ba96d68a2d635d93a8
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2024-02-26 22:17:31 +0100

    Ejercicio 1.0i versión 2

--- Clase2024/src/tema1/resueltos/ej1_0iV2/Ejercicio1_0iConObjetos.java
+++ Clase2024/src/tema1/resueltos/ej1_0iV2/Ejercicio1_0iConObjetos.java
@@ -1,43 +1,15 @@
-package tema1.resueltos;
+package tema1.resueltos.ej1_0iV2;
 
 public class Ejercicio1_0iConObjetos {
-	//	Crea dos arrays, uno con los nombres de 4 o 5 usuarios de cualquier red social que uses,
-	//	y otro con sus seguidores. Realiza un programa que muestre por consola esos usuarios 
-	//  primero sin ordenar y luego ordenados por número de seguidores 
-	//	(de mayor a menor).
 
 	public static void main(String[] args) {
-		// String[] usuarios = { "@sama", "@JeffBezos", "@BillGates", "@elonmusk" };
-		// int[] seguidores = { 1300, 61000, 62000, 128900 };
 		UsuarioRedSocial[] usuariosRS = new UsuarioRedSocial[4];
-		// Primera versión: con constructor por defecto
-		// usuariosRS[0] = new UsuarioRedSocial();
-		// usuariosRS[0].nombre = "@sams";
-		// usuariosRS[0].numSeguidores = 1300;
-		// usuariosRS[1] = new UsuarioRedSocial();
-		// usuariosRS[1].nombre = "@JeffBezos";
-		// usuariosRS[1].numSeguidores = 61000;
-		// usuariosRS[2] = new UsuarioRedSocial();
-		// usuariosRS[2].nombre = "@BillGates";
-		// usuariosRS[2].numSeguidores = 62000;
-		// usuariosRS[3] = new UsuarioRedSocial();
-		// usuariosRS[3].nombre = "@elonmusk";
-		// usuariosRS[3].numSeguidores = 128900;
-		// Segunda versión: con constructor con parámetros (mucho más cómodo):
 		usuariosRS[0] = new UsuarioRedSocial( "@sams", 1300 );
 		usuariosRS[1] = new UsuarioRedSocial( "@JeffBezos", 61000 );
 		usuariosRS[2] = new UsuarioRedSocial( "@BillGates", 62000 );
 		usuariosRS[3] = new UsuarioRedSocial( "@elonmusk", 128900 );
 		visualizarUsuariosYSeguidores( usuariosRS );
 		ordenaUsuariosPorSeguidores( usuariosRS );
-		visualizarUsuariosYSeguidores( usuariosRS );
-
-		// Prueba
-		UsuarioRedSocial u = new UsuarioRedSocial("Andoni");
-		System.out.println( u );
-		
-		// Baja de usuarios
-		bajaSeguidores( -2500, usuariosRS );
 		visualizarUsuariosYSeguidores( usuariosRS );
 	}
 
