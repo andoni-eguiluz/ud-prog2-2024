dc044c5bd62c068be974b1df2b034aa944a18b44
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2024-05-20 14:04:49 +0200

    Solución miniexamen

---
+++ Clase2024/src/varios/testExamenCortoResuelto/Concursable.java
@@ -0,0 +1,8 @@
+package varios.testExamenCortoResuelto;
+
+public interface Concursable {
+	/** Informa si el producto está en concurso o no
+	 * @return	true si lo está, false en caso contrario
+	 */
+	public boolean enConcurso();
+}
